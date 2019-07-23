a1e1d4d73c079ea290472c9343e17c50054f5f1e
ZTiKnl <ztik.nl@gmail.com>
2019-07-24 01:37:41 +0200

    switched from say to google tts

--- voice.js
+++ voice.js
@@ -1,8 +1,14 @@
 // set start vars
 var voiceactive = false;
+var voiceoutput;
 
-// include say module
-const say = require('say');
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
+
+const sfx = require('play-sound')(opts = {player: "aplay"})
 
 // include colored responses module
 const response = require('./response.js');
@@ -16,28 +22,49 @@ module.exports = {
       response.conlog('voice', 'speech synthesis was already activated', 'status');
     }
   },
-  synthesize: function(sentence) {
+  synthesize: async function(sentence) {
     if (voiceactive == true) {
-      say.speak(sentence, (err) => {
+      sentence = module.exports.vocalize(sentence);
+      const client = new textToSpeech.TextToSpeechClient({
+        projectId: 'sara-245106',
+        keyFilename: 'resources/apikeys/googlecloud.json'
+      })
+     
+      // Construct the request
+      const request = {
+        input: {text: sentence},
+        // Select the language and SSML Voice Gender (optional)
+        voice: {languageCode: 'en-US', name: 'en-GB-Wavenet-C', ssmlGender: 'FEMALE'},
+        // Select the type of audio encoding
+        audioConfig: {audioEncoding: 'LINEAR16'},
+      };
+     
+      // Performs the Text-to-Speech request
+      const [result] = await client.synthesizeSpeech(request);
+      // Write the binary audio content to a local file
+      const writeFile = util.promisify(fs.writeFile);
+      await writeFile('resources/voice/output.wav', result.audioContent, 'binary');
+
+      voiceoutput = sfx.play('./resources/voice/output.wav', function(err){
         if (err) {
-          response.conlog('voice.synthesize', err.message, 'error');
+          response.conlog('voice', 'Couldn\'t synthesize voice: '+err.message, 'error');
           return
         }
-        response.conlog('voice.synthesize', sentence, 'response');
-        return
-      });
+        voiceoutput = null;
+      })
     }
   },
   vocalize: function(sentence) {
     // todo: replace input with vocal corrections (ztik -> stick, S.A.R.A. -> sarah)
+    return sentence;
   },
   silence: function() {
-    say.stop(function (err) {
-      if (err) {
-        console.log('error: '+err.message)
-      }
-      console.log('stopped talking')
-    });
+    if (voiceoutput != null) {
+      voiceoutput.kill();
+      response.conlog('voice', 'Stopped talking', 'info');
+    } else {
+      response.conlog('voice', 'I wasn\'t talking (anymore)', 'info');
+    }
   },
   stop: function() {
     if (voiceactive == true) {
