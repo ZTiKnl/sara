ccb1846f6864f04b2444c4f1f4c8c11df60ab7bb
ZTiKnl <ztik.nl@gmail.com>
2019-08-06 05:15:12 +0200

    rewrote plugin loader system

--- plugins.js
+++ plugins.js
@@ -25,26 +25,27 @@ function plugincheck (line) {
         .filter(file => file.endsWith('.json'))
         .map(file => load(`plugins/${file}`));
 
-      plugins.forEach(plugindata => {
-        const plugin = load(`plugins/${plugindata.module}.js`);
-        const fn = plugin[plugindata.name];
-        var result = eval(plugindata.regex).exec(line)
+      var i = 0;
+      var amount = plugins.length;
+      while (i < amount) {
+        var result = eval(plugins[i]['regex']).exec(line);
         if (result) {
-          result.forEach(async function(element) {
-            var matches = element.match(eval(plugindata.regex));
-            if (matches !== null) {
-              pluginfound = true;
-              response.conlog('plugincheck', 'found match: '+plugindata.module+'.'+plugindata.name, 'data');
-              matches.forEach(function(argument) {
-                argumentarray.push(argument);
-              })
-              let rslt = fn(argumentarray);
-              resolve(rslt);
-              return;
-            }
-          });
+          pluginfound = true;
+          response.conlog('plugincheck', 'found match: '+plugins[i]['module']+'.'+plugins[i]['name'], 'data');
+          
+          const plugin = load('plugins/'+plugins[i]['module']+'.js');
+          const fn = plugin[plugins[i]['name']];
+          let rslt = fn(result);
+          resolve(rslt);
         }
-      });
+        
+        if (result) {
+          i = amount;
+        } else {
+          i++;
+        }
+      }
+
       if (pluginfound == false) {
         response.conlog('pluginloader', 'Couldn\'t find matching plugin for command: '+line, 'error');
       }
