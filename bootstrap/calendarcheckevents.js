158e26c0cf4973c0e847e13121a3c15f9518c852
ZTiKnl <ztik.nl@gmail.com>
2019-08-20 19:29:04 +0200

    added status function

--- bootstrap/calendarcheckevents.js
+++ bootstrap/calendarcheckevents.js
@@ -1,14 +1,30 @@
 // include colored responses module
 const response = require('../response.js');
+
 var daemon;
+var bootstrapactive = false;
+
 module.exports = {
   start: function() {
-    response.conlog('bootstrap', 'calendar event checker activated', 'status');
-    daemon = setInterval(calsync,10000);
+    if (!bootstrapactive) {
+      response.conlog('bootstrap', 'calendar event checker activated', 'status');
+      daemon = setInterval(calsync,10000);
+      bootstrapactive = true;
+    } else {
+      response.conlog('bootstrap', 'calendar event checker was already activated', 'status');
+    }
   },
   stop: function() {
-    response.conlog('bootstrap', 'calendar event checker deactivated', 'status');
-    clearInterval(daemon);
+    if (bootstrapactive) {
+      response.conlog('bootstrap', 'calendar event checker deactivated', 'status');
+      clearInterval(daemon);
+      bootstrapactive = false;
+    } else {
+      response.conlog('bootstrap', 'calendar event checker was already activated', 'status');
+    }
+  },
+  status: function() {
+    return bootstrapactive;
   }
 }
 
